841629bdcf336f060006ca83b002a9cef47f0529
wzhehong <84788737+wzhehong@users.noreply.github.com>
2022-07-22 21:42:10 -0400

    Add files via upload

--- verilog/detection_unit.sv
+++ verilog/detection_unit.sv
@@ -24,27 +24,8 @@ module detection_unit(
 
     output logic [1:0] rollback
 
-`ifdef DETECTION_UNIT_TEST
-    ,
-    output FORWARDING_TYPE [2:0] forwarding_A,
-    output FORWARDING_TYPE [2:0] forwarding_B
-`endif
-
 );
 
-`ifdef DETECTION_UNIT_TEST
-    assign forwarding_A = {
-        id_packet_out_0.forwarding_A,
-        id_packet_out_1.forwarding_A,
-        id_packet_out_2.forwarding_A
-    };
-
-    assign forwarding_B = {
-        id_packet_out_0.forwarding_B,
-        id_packet_out_1.forwarding_B,
-        id_packet_out_2.forwarding_B
-    };
-`endif
 
     logic [4:0] id_reg_A_0;
     assign id_reg_A_0 = id_packet_0.inst.r.rs1;
@@ -111,135 +92,135 @@ module detection_unit(
 
     always_comb begin
         id_packet_out_0 = id_packet_0;
-        id_packet_out_0.forwarding_A = RS_IS_RS;
-        id_packet_out_0.forwarding_B = RS_IS_RS;
+        id_packet_out_0.rs1_select = RS_IS_RS;
+        id_packet_out_0.rs2_select = RS_IS_RS;
 
         if (id_reg_A_0 == ex_dest_reg_2 && !id_reg_A_ZERO_0) begin
-            id_packet_out_0.forwarding_A = RS_IS_EX_2;
+            id_packet_out_0.rs1_select = RS_IS_EX_2;
         end
         else if (id_reg_A_0 == ex_dest_reg_1 && !id_reg_A_ZERO_0) begin
-            id_packet_out_0.forwarding_A = RS_IS_EX_1;
+            id_packet_out_0.rs1_select = RS_IS_EX_1;
         end
         else if (id_reg_A_0 == ex_dest_reg_0 && !id_reg_A_ZERO_0) begin
-            id_packet_out_0.forwarding_A = RS_IS_EX_0;
+            id_packet_out_0.rs1_select = RS_IS_EX_0;
         end
         
         if (id_reg_B_0 == ex_dest_reg_2 && !id_reg_B_ZERO_0) begin
-            id_packet_out_0.forwarding_B = RS_IS_EX_2;
+            id_packet_out_0.rs2_select = RS_IS_EX_2;
         end
         else if (id_reg_B_0 == ex_dest_reg_1 && !id_reg_B_ZERO_0) begin
-            id_packet_out_0.forwarding_B = RS_IS_EX_1;
+            id_packet_out_0.rs2_select = RS_IS_EX_1;
         end
         else if (id_reg_B_0 == ex_dest_reg_0 && !id_reg_B_ZERO_0) begin
-            id_packet_out_0.forwarding_B = RS_IS_EX_0;
+            id_packet_out_0.rs2_select = RS_IS_EX_0;
         end
 
         if (id_reg_A_0 == mem_dest_reg_2 && !id_reg_A_ZERO_0) begin
-            id_packet_out_0.forwarding_A = RS_IS_MEM_2;
+            id_packet_out_0.rs1_select = RS_IS_MEM_2;
         end
         else if (id_reg_A_0 == mem_dest_reg_1 && !id_reg_A_ZERO_0) begin
-            id_packet_out_0.forwarding_A = RS_IS_MEM_1;
+            id_packet_out_0.rs1_select = RS_IS_MEM_1;
         end
         else if (id_reg_A_0 == mem_dest_reg_0 && !id_reg_A_ZERO_0) begin
-            id_packet_out_0.forwarding_A = RS_IS_MEM_0;
+            id_packet_out_0.rs1_select = RS_IS_MEM_0;
         end
 
         if (id_reg_B_0 == mem_dest_reg_2 && !id_reg_B_ZERO_0) begin
-            id_packet_out_0.forwarding_B = RS_IS_MEM_2;
+            id_packet_out_0.rs2_select = RS_IS_MEM_2;
         end
         else if (id_reg_B_0 == mem_dest_reg_1 && !id_reg_B_ZERO_0) begin
-            id_packet_out_0.forwarding_B = RS_IS_MEM_1;
+            id_packet_out_0.rs2_select = RS_IS_MEM_1;
         end
         else if (id_reg_B_0 == mem_dest_reg_0 && !id_reg_B_ZERO_0) begin
-            id_packet_out_0.forwarding_B = RS_IS_MEM_0;
+            id_packet_out_0.rs2_select = RS_IS_MEM_0;
         end
 
         id_packet_out_1 = id_packet_1;
-        id_packet_out_1.forwarding_A = RS_IS_RS;
-        id_packet_out_1.forwarding_B = RS_IS_RS;
+        id_packet_out_1.rs1_select = RS_IS_RS;
+        id_packet_out_1.rs2_select = RS_IS_RS;
 
         if (id_reg_A_1 == ex_dest_reg_2 && !id_reg_A_ZERO_1) begin
-            id_packet_out_1.forwarding_A = RS_IS_EX_2;
+            id_packet_out_1.rs1_select = RS_IS_EX_2;
         end
         else if (id_reg_A_1 == ex_dest_reg_1 && !id_reg_A_ZERO_1) begin
-            id_packet_out_1.forwarding_A = RS_IS_EX_1;
+            id_packet_out_1.rs1_select = RS_IS_EX_1;
         end
         else if (id_reg_A_1 == ex_dest_reg_0 && !id_reg_A_ZERO_1) begin
-            id_packet_out_1.forwarding_A = RS_IS_EX_0;
+            id_packet_out_1.rs1_select = RS_IS_EX_0;
         end
 
         if (id_reg_B_1 == ex_dest_reg_2 && !id_reg_B_ZERO_1) begin
-            id_packet_out_1.forwarding_B = RS_IS_EX_2;
+            id_packet_out_1.rs2_select = RS_IS_EX_2;
         end
         else if (id_reg_B_1 == ex_dest_reg_1 && !id_reg_B_ZERO_1) begin
-            id_packet_out_1.forwarding_B = RS_IS_EX_1;
+            id_packet_out_1.rs2_select = RS_IS_EX_1;
         end
         else if (id_reg_B_1 == ex_dest_reg_0 && !id_reg_B_ZERO_1) begin
-            id_packet_out_1.forwarding_B = RS_IS_EX_0;
+            id_packet_out_1.rs2_select = RS_IS_EX_0;
         end
 
         if (id_reg_A_1 == mem_dest_reg_2 && !id_reg_A_ZERO_1) begin
-            id_packet_out_1.forwarding_A = RS_IS_MEM_2;
+            id_packet_out_1.rs1_select = RS_IS_MEM_2;
         end
         else if (id_reg_A_1 == mem_dest_reg_1 && !id_reg_A_ZERO_1) begin
-            id_packet_out_1.forwarding_A = RS_IS_MEM_1;
+            id_packet_out_1.rs1_select = RS_IS_MEM_1;
         end
         else if (id_reg_A_1 == mem_dest_reg_0 && !id_reg_A_ZERO_1) begin
-            id_packet_out_1.forwarding_A = RS_IS_MEM_0;
+            id_packet_out_1.rs1_select = RS_IS_MEM_0;
         end
 
         if (id_reg_B_1 == mem_dest_reg_2 && !id_reg_B_ZERO_1) begin
-            id_packet_out_1.forwarding_B = RS_IS_MEM_2;
+            id_packet_out_1.rs2_select = RS_IS_MEM_2;
         end
         else if (id_reg_B_1 == mem_dest_reg_1 && !id_reg_B_ZERO_1) begin
-            id_packet_out_1.forwarding_B = RS_IS_MEM_1;
+            id_packet_out_1.rs2_select = RS_IS_MEM_1;
         end
         else if (id_reg_B_1 == mem_dest_reg_0 && !id_reg_B_ZERO_1) begin
-            id_packet_out_1.forwarding_B = RS_IS_MEM_0;
+            id_packet_out_1.rs2_select = RS_IS_MEM_0;
         end
 
         id_packet_out_2 = id_packet_2;
-        id_packet_out_2.forwarding_A = RS_IS_RS;
-        id_packet_out_2.forwarding_B = RS_IS_RS;
+        id_packet_out_2.rs1_select = RS_IS_RS;
+        id_packet_out_2.rs2_select = RS_IS_RS;
 
         if (id_reg_A_2 == ex_dest_reg_2 && !id_reg_A_ZERO_2) begin
-            id_packet_out_2.forwarding_A = RS_IS_EX_2;
+            id_packet_out_2.rs1_select = RS_IS_EX_2;
         end
         else if (id_reg_A_2 == ex_dest_reg_1 && !id_reg_A_ZERO_2) begin
-            id_packet_out_2.forwarding_A = RS_IS_EX_1;
+            id_packet_out_2.rs1_select = RS_IS_EX_1;
         end
         else if (id_reg_A_2 == ex_dest_reg_0 && !id_reg_A_ZERO_2) begin
-            id_packet_out_2.forwarding_A = RS_IS_EX_0;
+            id_packet_out_2.rs1_select = RS_IS_EX_0;
         end
 
         if (id_reg_B_2 == ex_dest_reg_2 && !id_reg_B_ZERO_2) begin
-            id_packet_out_2.forwarding_B = RS_IS_EX_2;
+            id_packet_out_2.rs2_select = RS_IS_EX_2;
         end
         else if (id_reg_B_2 == ex_dest_reg_1 && !id_reg_B_ZERO_2) begin
-            id_packet_out_2.forwarding_B = RS_IS_EX_1;
+            id_packet_out_2.rs2_select = RS_IS_EX_1;
         end
         else if (id_reg_B_2 == ex_dest_reg_0 && !id_reg_B_ZERO_2) begin
-            id_packet_out_2.forwarding_B = RS_IS_EX_0;
+            id_packet_out_2.rs2_select = RS_IS_EX_0;
         end
 
         if (id_reg_A_2 == mem_dest_reg_2 && !id_reg_A_ZERO_2) begin
-            id_packet_out_2.forwarding_A = RS_IS_MEM_2;
+            id_packet_out_2.rs1_select = RS_IS_MEM_2;
         end
         else if (id_reg_A_2 == mem_dest_reg_1 && !id_reg_A_ZERO_2) begin
-            id_packet_out_2.forwarding_A = RS_IS_MEM_1;
+            id_packet_out_2.rs1_select = RS_IS_MEM_1;
         end
         else if (id_reg_A_2 == mem_dest_reg_0 && !id_reg_A_ZERO_2) begin
-            id_packet_out_2.forwarding_A = RS_IS_MEM_0;
+            id_packet_out_2.rs1_select = RS_IS_MEM_0;
         end
 
         if (id_reg_B_2 == mem_dest_reg_2 && !id_reg_B_ZERO_2) begin
-            id_packet_out_2.forwarding_B = RS_IS_MEM_2;
+            id_packet_out_2.rs2_select = RS_IS_MEM_2;
         end
         else if (id_reg_B_2 == mem_dest_reg_1 && !id_reg_B_ZERO_2) begin
-            id_packet_out_2.forwarding_B = RS_IS_MEM_1;
+            id_packet_out_2.rs2_select = RS_IS_MEM_1;
         end
         else if (id_reg_B_2 == mem_dest_reg_0 && !id_reg_B_ZERO_2) begin
-            id_packet_out_2.forwarding_B = RS_IS_MEM_0;
+            id_packet_out_2.rs2_select = RS_IS_MEM_0;
         end
 
     end
