47a9adf72afbd10f7ed25675bd510f861f6d3e8c
Tianchen Ye <82188210+jackye666@users.noreply.github.com>
2022-07-24 19:54:13 -0400

    update testbench

--- testbench/testbench.sv
+++ testbench/testbench.sv
@@ -9,7 +9,7 @@
 /////////////////////////////////////////////////////////////////////////
 
 `timescale 1ns/100ps
-
+`include "sys_defs.svh"
 import "DPI-C" function void print_header(string str);
 import "DPI-C" function void print_cycles();
 import "DPI-C" function void print_stage(string div, int inst, int npc, int valid_inst);
@@ -30,38 +30,40 @@ module testbench;
 	logic [31:0] instr_count;
 	int          wb_fileno;
 	
-	logic [1:0]  proc2mem_command;
-	logic [`XLEN-1:0] proc2mem_addr;
-	logic [63:0] proc2mem_data;
-	logic  [3:0] mem2proc_response;
-	logic [63:0] mem2proc_data;
-	logic  [3:0] mem2proc_tag;
-`ifndef CACHE_MODE
-	MEM_SIZE     proc2mem_size;
-`endif
+	logic [1:0]  proc2mem_command [`WAYS:0];
+	logic [`XLEN-1:0] proc2mem_addr [`WAYS:0];
+	logic [63:0] proc2mem_data [`WAYS:0];
+
+	logic  [3:0] mem2proc_response [`WAYS:0];
+	logic [63:0] mem2proc_data [`WAYS:0];
+	logic  [3:0] mem2proc_tag [`WAYS:0];
+
+	MEM_SIZE     proc2mem_size [`WAYS:0];
+
 	logic  [3:0] pipeline_completed_insts;
 	EXCEPTION_CODE   pipeline_error_status;
+
 	logic  [4:0] pipeline_commit_wr_idx;
 	logic [`XLEN-1:0] pipeline_commit_wr_data;
 	logic        pipeline_commit_wr_en;
 	logic [`XLEN-1:0] pipeline_commit_NPC;
-	
-	
-	logic [`XLEN-1:0] if_NPC_out;
-	logic [31:0] if_IR_out;
-	logic        if_valid_inst_out;
-	logic [`XLEN-1:0] if_id_NPC;
-	logic [31:0] if_id_IR;
-	logic        if_id_valid_inst;
-	logic [`XLEN-1:0] id_ex_NPC;
-	logic [31:0] id_ex_IR;
-	logic        id_ex_valid_inst;
-	logic [`XLEN-1:0] ex_mem_NPC;
-	logic [31:0] ex_mem_IR;
-	logic        ex_mem_valid_inst;
-	logic [`XLEN-1:0] mem_wb_NPC;
-	logic [31:0] mem_wb_IR;
-	logic        mem_wb_valid_inst;
+
+	logic [`XLEN-1:0] if_NPC_out [`WAYS:0];
+	logic [31:0] if_IR_out [`WAYS:0];
+	logic        if_valid_inst_out [`WAYS:0];
+	logic [`XLEN-1:0] if_id_NPC [`WAYS:0];
+	logic [31:0] if_id_IR [`WAYS:0];
+	logic        if_id_valid_inst [`WAYS:0];
+	logic [`XLEN-1:0] id_ex_NPC [`WAYS:0];
+	logic [31:0] id_ex_IR [`WAYS:0];
+	logic        id_ex_valid_inst [`WAYS:0];
+	logic [`XLEN-1:0] ex_mem_NPC [`WAYS:0];
+	logic [31:0] ex_mem_IR [`WAYS:0];
+	logic        ex_mem_valid_inst [`WAYS:0];
+	logic [`XLEN-1:0] mem_wb_NPC [`WAYS:0];
+	logic [31:0] mem_wb_IR [`WAYS:0];
+	logic        mem_wb_valid_inst [`WAYS:0];
+
 
     //counter used for when pipeline infinite loops, forces termination
     logic [63:0] debug_counter;
@@ -83,11 +85,12 @@ module testbench;
 		
 		.pipeline_completed_insts(pipeline_completed_insts),
 		.pipeline_error_status(pipeline_error_status),
+
 		.pipeline_commit_wr_data(pipeline_commit_wr_data),
 		.pipeline_commit_wr_idx(pipeline_commit_wr_idx),
 		.pipeline_commit_wr_en(pipeline_commit_wr_en),
 		.pipeline_commit_NPC(pipeline_commit_NPC),
-		
+
 		.if_NPC_out(if_NPC_out),
 		.if_IR_out(if_IR_out),
 		.if_valid_inst_out(if_valid_inst_out),
@@ -102,28 +105,37 @@ module testbench;
 		.ex_mem_valid_inst(ex_mem_valid_inst),
 		.mem_wb_NPC(mem_wb_NPC),
 		.mem_wb_IR(mem_wb_IR),
-		.mem_wb_valid_inst(mem_wb_valid_inst)
+		.mem_wb_valid_inst(mem_wb_valid_inst)	
 	);
-	
-	
-	// Instantiate the Data Memory
-	mem memory (
-		// Inputs
-		.clk               (clock),
-		.proc2mem_command  (proc2mem_command),
-		.proc2mem_addr     (proc2mem_addr),
-		.proc2mem_data     (proc2mem_data),
-`ifndef CACHE_MODE
-		.proc2mem_size     (proc2mem_size),
-`endif
 
-		// Outputs
 
-		.mem2proc_response (mem2proc_response),
-		.mem2proc_data     (mem2proc_data),
-		.mem2proc_tag      (mem2proc_tag)
-	);
+	mem memory(
+	.clk,              // Memory clock
+	.proc2mem_addr_0(proc2mem_addr[0]),    // address for current command
+	.proc2mem_addr_1(proc2mem_addr[1]),    // address for current command
+	.proc2mem_addr_2(proc2mem_addr[2]),    // address for current command
+	//support for memory model with byte level addressing
+	.proc2mem_data_0(proc2mem_data[0]),    // address for current command
+	.proc2mem_data_1(proc2mem_data[1]),    // address for current command
+	.proc2mem_data_2(proc2mem_data[2]),    // address for current command
+
+	.proc2mem_size_0(proc2mem_size[0]), //BYTE, HALF, WORD or DOUBLE
+	.proc2mem_size_1(proc2mem_size[1]), //BYTE, HALF, WORD or DOUBLE
+	.proc2mem_size_2(proc2mem_size[2]), //BYTE, HALF, WORD or DOUBLE
+	.proc2mem_command_0(proc2mem_command[0]), // `BUS_NONE `BUS_LOAD or `BUS_STORE
+	.proc2mem_command_1(proc2mem_command[1]), // `BUS_NONE `BUS_LOAD or `BUS_STORE
+	.proc2mem_command_2(proc2mem_command[2]), // `BUS_NONE `BUS_LOAD or `BUS_STORE
 	
+	.mem2proc_response_0(mem2proc_response[0]),// 0 = can't accept, other=tag of transaction
+	.mem2proc_response_1(mem2proc_response[1]),// 0 = can't accept, other=tag of transaction
+	.mem2proc_response_2(mem2proc_response[2]),// 0 = can't accept, other=tag of transaction
+	.mem2proc_data_0(mem2proc_data[0]),    // data resulting from a load
+	.mem2proc_data_1(mem2proc_data[1]),    // data resulting from a load
+	.mem2proc_data_2(mem2proc_data[2]),    // data resulting from a load
+	.mem2proc_tag_0(mem2proc_tag[0]),    // 0 = no value, other=tag of transaction
+	.mem2proc_tag_1(mem2proc_tag[1]),    // 0 = no value, other=tag of transaction
+	.mem2proc_tag_2(mem2proc_tag[2])     // 0 = no value, other=tag of transaction
+);	
 	// Generate System Clock
 	always begin
 		#(`VERILOG_CLOCK_PERIOD/2.0);
@@ -190,7 +202,7 @@ module testbench;
 		
 		//Open header AFTER throwing the reset otherwise the reset state is displayed
 		print_header("                                                                            D-MEM Bus &\n");
-		print_header("Cycle:      IF      |     ID      |     EX      |     MEM     |     WB      Reg Result");
+		print_header("Cycle:      IF1     |     IF2     |     IF3     |     ID1     |     ID2     |     ID3     |     MEM1    |     MEM2    |     MEM3    |     EX1     |     EX2     |     EX3     |     WB1     |     WB2     |     WB3     ");
 	end
 
 
@@ -218,27 +230,57 @@ module testbench;
 			
 			 // print the piepline stuff via c code to the pipeline.out
 			 print_cycles();
-			 print_stage(" ", if_IR_out, if_NPC_out[31:0], {31'b0,if_valid_inst_out});
-			 print_stage("|", if_id_IR, if_id_NPC[31:0], {31'b0,if_id_valid_inst});
-			 print_stage("|", id_ex_IR, id_ex_NPC[31:0], {31'b0,id_ex_valid_inst});
-			 print_stage("|", ex_mem_IR, ex_mem_NPC[31:0], {31'b0,ex_mem_valid_inst});
-			 print_stage("|", mem_wb_IR, mem_wb_NPC[31:0], {31'b0,mem_wb_valid_inst});
+			 print_stage(" ", if_IR_out[0], if_NPC_out[0][31:0], {31'b0,if_valid_inst_out[0]});
+			 print_stage("|", if_IR_out[1], if_NPC_out[1][31:0], {31'b0,if_valid_inst_out[1]});
+			 print_stage("|", if_IR_out[2], if_NPC_out[2][31:0], {31'b0,if_valid_inst_out[2]});
+
+			 print_stage("|", if_id_IR[0], if_id_NPC[0][31:0], {31'b0,if_id_valid_inst[0]});
+			 print_stage("|", if_id_IR[1], if_id_NPC[1][31:0], {31'b0,if_id_valid_inst[1]});
+			 print_stage("|", if_id_IR[2], if_id_NPC[2][31:0], {31'b0,if_id_valid_inst[2]});
+
+			 print_stage("|", id_ex_IR[0], id_ex_NPC[0][31:0], {31'b0,id_ex_valid_inst[0]});
+			 print_stage("|", id_ex_IR[1], id_ex_NPC[1][31:0], {31'b0,id_ex_valid_inst[1]});
+			 print_stage("|", id_ex_IR[2], id_ex_NPC[2][31:0], {31'b0,id_ex_valid_inst[2]});
+
+			 print_stage("|", ex_mem_IR[0], ex_mem_NPC[0][31:0], {31'b0,ex_mem_valid_inst[0]});
+			 print_stage("|", ex_mem_IR[1], ex_mem_NPC[1][31:0], {31'b0,ex_mem_valid_inst[1]});
+			 print_stage("|", ex_mem_IR[2], ex_mem_NPC[2][31:0], {31'b0,ex_mem_valid_inst[2]});
+
+			 print_stage("|", mem_wb_IR[0], mem_wb_NPC[0][31:0], {31'b0,mem_wb_valid_inst[0]});
+			 print_stage("|", mem_wb_IR[1], mem_wb_NPC[1][31:0], {31'b0,mem_wb_valid_inst[1]});
+			 print_stage("|", mem_wb_IR[2], mem_wb_NPC[2][31:0], {31'b0,mem_wb_valid_inst[2]});
+
+			 /*
 			 print_reg(32'b0, pipeline_commit_wr_data[31:0],
 				{27'b0,pipeline_commit_wr_idx}, {31'b0,pipeline_commit_wr_en});
 			 print_membus({30'b0,proc2mem_command}, {28'b0,mem2proc_response},
 				32'b0, proc2mem_addr[31:0],
 				proc2mem_data[63:32], proc2mem_data[31:0]);
-			
+			*/
 			
 			 // print the writeback information to writeback.out
 			if(pipeline_completed_insts>0) begin
-				if(pipeline_commit_wr_en)
+				if(pipeline_commit_wr_en[0])
 					$fdisplay(wb_fileno, "PC=%x, REG[%d]=%x",
-						pipeline_commit_NPC-4,
-						pipeline_commit_wr_idx,
-						pipeline_commit_wr_data);
+						pipeline_commit_NPC[0]-4,
+						pipeline_commit_wr_idx[0],
+						pipeline_commit_wr_data[0]);
 				else
-					$fdisplay(wb_fileno, "PC=%x, ---",pipeline_commit_NPC-4);
+					$fdisplay(wb_fileno, "PC=%x, ---",pipeline_commit_NPC[1]-4);
+				if(pipeline_commit_wr_en[1])
+					$fdisplay(wb_fileno, "PC=%x, REG[%d]=%x",
+						pipeline_commit_NPC[1]-4,
+						pipeline_commit_wr_idx[1],
+						pipeline_commit_wr_data[1]);
+				else
+					$fdisplay(wb_fileno, "PC=%x, ---",pipeline_commit_NPC[1]-4);
+				if(pipeline_commit_wr_en[2])
+					$fdisplay(wb_fileno, "PC=%x, REG[%d]=%x",
+						pipeline_commit_NPC[2]-4,
+						pipeline_commit_wr_idx[2],
+						pipeline_commit_wr_data[2]);
+				else
+					$fdisplay(wb_fileno, "PC=%x, ---",pipeline_commit_NPC[2]-4);
 			end
 			
 			// deal with any halting conditions
