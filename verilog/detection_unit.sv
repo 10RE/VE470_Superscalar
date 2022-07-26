942b9dbebef0bc4470236bb833506bb53f4bf95f
flandre <flandre@umich.edu>
2022-07-26 16:49:34 -0400

    modualized dection unit

--- verilog/detection_unit.sv
+++ verilog/detection_unit.sv
@@ -5,6 +5,8 @@
 
 //`define DETECTION_UNIT_TEST
 
+
+
 module detection_unit(
     input ID_EX_PACKET id_packet_0,
     input ID_EX_PACKET id_packet_1,
@@ -31,6 +33,79 @@ module detection_unit(
 `endif
 
 );
+    logic [4:0] mem_dest_reg [2:0];
+    assign mem_dest_reg={
+        mem_packet_2.dest_reg_idx,
+        mem_packet_1.dest_reg_idx,
+        mem_packet_0.dest_reg_idx
+    };
+
+    logic [4:0] ex_dest_reg [2:0];
+    assign ex_dest_reg={
+        ex_packet_2.dest_reg_idx,
+        ex_packet_1.dest_reg_idx,
+        ex_packet_0.dest_reg_idx
+    };
+
+    logic ex_load [2:0];
+    assign ex_load={
+        ex_packet_2.rd_mem,
+        ex_packet_1.rd_mem,
+        ex_packet_0.rd_mem
+    };
+
+module single_rs_select(
+    input logic [4:0] id_reg,
+    output RS_SELECT rs_select,
+    output logic ex_load_depend
+);
+    RS_SELECT rs_select_ex;
+    always_comb begin
+        ex_load_depend=0;
+        rs_select = RS_IS_RS;
+        if (id_reg!=`ZERO_REG) begin
+            rs_select = (id_reg==ex_dest_reg[2])?RS_IS_EX_2:
+                        (id_reg==ex_dest_reg[1])?RS_IS_EX_1:
+                        (id_reg==ex_dest_reg[0])?RS_IS_EX_0:
+                        (id_reg==mem_dest_reg[2])?RS_IS_MEM_2:
+                        (id_reg==mem_dest_reg[1])?RS_IS_MEM_1:
+                        (id_reg==mem_dest_reg[0])?RS_IS_MEM_0:
+                        RS_IS_RS;
+            ex_load_depend =    (id_reg==ex_dest_reg[2]&&ex_load[2])||
+                                (id_reg==ex_dest_reg[1]&&ex_load[1])||
+                                (id_reg==ex_dest_reg[0]&&ex_load[0]);
+        end
+    end
+endmodule
+
+module single_way_select(
+    input ID_EX_PACKET id_packet,
+    output ID_EX_PACKET id_packet_out,
+    output logic ex_load_depend
+);
+    RS_SELECT rs1_select,rs2_select;
+    logic rs1_ex_load_depend,rs2_ex_load_depend;
+
+    single_rs_select rs1(
+        .id_reg(id_packet.inst.r.rs1),
+        .rs_select(rs1_select),
+        .ex_load_depend(rs1_ex_load_depend)
+    );
+    single_rs_select rs2(
+        .id_reg(id_packet.inst.r.rs2),
+        .rs_select(rs2_select),
+        .ex_load_depend(rs2_ex_load_depend)
+    );
+    assign ex_load_depend = rs1_ex_load_depend || rs2_ex_load_depend;
+
+    always_comb begin
+        id_packet_out = id_packet;
+        id_packet_out.rs1_select = rs1_select;
+        id_packet_out.rs2_select = rs2_select;
+    end
+endmodule
+
+
 
 `ifdef DETECTION_UNIT_TEST
     assign forwarding_A = {
@@ -45,265 +120,75 @@ module detection_unit(
         id_packet_out_2.rs2_select
     };
 `endif
+    logic ex_load_depend_0, ex_load_depend_1, ex_load_depend_2;
 
-    logic [4:0] id_reg_A_0;
-    assign id_reg_A_0 = id_packet_0.inst.r.rs1;
+    single_way_select way_0(
+        .id_packet(id_packet_0),
+        .id_packet_out(id_packet_out_0),
+        .ex_load_depend(ex_load_depend_0)
+    );
+    single_way_select way_1(
+        .id_packet(id_packet_1),
+        .id_packet_out(id_packet_out_1),
+        .ex_load_depend(ex_load_depend_1)
+    );
+    single_way_select way_2(
+        .id_packet(id_packet_2),
+        .id_packet_out(id_packet_out_2),
+        .ex_load_depend(ex_load_depend_2)
+    );
 
-    logic [4:0] id_reg_B_0;
-    assign id_reg_B_0 = id_packet_0.inst.r.rs2;
+module depend_helper(
+    input ID_EX_PACKET id_packet,
+    input ID_EX_PACKET id_packet_later,
+    output logic depend
+);
+    assign depend = (id_packet.dest_reg_idx == id_packet_later.inst.r.rs1 && id_packet_later.inst.r.rs1!=`ZERO_REG) || 
+                    (id_packet.dest_reg_idx == id_packet_later.inst.r.rs2 && id_packet_later.inst.r.rs2!=`ZERO_REG);
+endmodule
 
-    logic [4:0] id_reg_A_1;
-    assign id_reg_A_1 = id_packet_1.inst.r.rs1;
+    logic way_0_depend_by_1, way_0_depend_by_2, way_1_depend_by_2;
 
-    logic [4:0] id_reg_B_1;
-    assign id_reg_B_1 = id_packet_1.inst.r.rs2;
+    depend_helper way0vs1(
+        .id_packet(id_packet_0),
+        .id_packet_later(id_packet_1),
+        .depend(way_0_depend_by_1)
+    );
+    depend_helper way0vs2(
+        .id_packet(id_packet_0),
+        .id_packet_later(id_packet_2),
+        .depend(way_0_depend_by_2)
+    );
+    depend_helper way1vs2(
+        .id_packet(id_packet_1),
+        .id_packet_later(id_packet_2),
+        .depend(way_1_depend_by_2)
+    );
 
-    logic [4:0] id_reg_A_2;
-    assign id_reg_A_2 = id_packet_2.inst.r.rs1;
-
-    logic [4:0] id_reg_B_2;
-    assign id_reg_B_2 = id_packet_2.inst.r.rs2;
-
-    logic [4:0] id_dest_reg_0;
-    assign id_dest_reg_0 = id_packet_0.dest_reg_idx;
-
-    logic [4:0] id_dest_reg_1;
-    assign id_dest_reg_1 = id_packet_1.dest_reg_idx;
-
-    logic [4:0] id_dest_reg_2;
-    assign id_dest_reg_2 = id_packet_2.dest_reg_idx;
-
-    logic [4:0] ex_dest_reg_0;
-    assign ex_dest_reg_0 = ex_packet_0.dest_reg_idx;
-
-    logic [4:0] ex_dest_reg_1;
-    assign ex_dest_reg_1 = ex_packet_1.dest_reg_idx;
-
-    logic [4:0] ex_dest_reg_2;
-    assign ex_dest_reg_2 = ex_packet_2.dest_reg_idx;
-
-    logic [4:0] mem_dest_reg_0;
-    assign mem_dest_reg_0 = mem_packet_0.dest_reg_idx;
-
-    logic [4:0] mem_dest_reg_1;
-    assign mem_dest_reg_1 = mem_packet_1.dest_reg_idx;
-
-    logic [4:0] mem_dest_reg_2;
-    assign mem_dest_reg_2 = mem_packet_2.dest_reg_idx;
-
-    logic id_reg_A_ZERO_0;
-    assign id_reg_A_ZERO_0 = (id_packet_0.inst.r.rs1 == `ZERO_REG);
-
-    logic id_reg_B_ZERO_0;
-    assign id_reg_B_ZERO_0 = (id_packet_0.inst.r.rs2 == `ZERO_REG);
-
-    logic id_reg_A_ZERO_1;
-    assign id_reg_A_ZERO_1 = (id_packet_1.inst.r.rs1 == `ZERO_REG);
-
-    logic id_reg_B_ZERO_1;
-    assign id_reg_B_ZERO_1 = (id_packet_1.inst.r.rs2 == `ZERO_REG);
-
-    logic id_reg_A_ZERO_2;
-    assign id_reg_A_ZERO_2 = (id_packet_2.inst.r.rs1 == `ZERO_REG);
-
-    logic id_reg_B_ZERO_2;
-    assign id_reg_B_ZERO_2 = (id_packet_2.inst.r.rs2 == `ZERO_REG);
-
-    always_comb begin
-        id_packet_out_0 = id_packet_0;
-        id_packet_out_0.rs1_select = RS_IS_RS;
-        id_packet_out_0.rs2_select = RS_IS_RS;
-        
-        if (id_reg_A_0 == mem_dest_reg_2 && !id_reg_A_ZERO_0) begin
-            id_packet_out_0.rs1_select = RS_IS_MEM_2;
-        end
-        else if (id_reg_A_0 == mem_dest_reg_1 && !id_reg_A_ZERO_0) begin
-            id_packet_out_0.rs1_select = RS_IS_MEM_1;
-        end
-        else if (id_reg_A_0 == mem_dest_reg_0 && !id_reg_A_ZERO_0) begin
-            id_packet_out_0.rs1_select = RS_IS_MEM_0;
-        end
-
-        if (id_reg_B_0 == mem_dest_reg_2 && !id_reg_B_ZERO_0) begin
-            id_packet_out_0.rs2_select = RS_IS_MEM_2;
-        end
-        else if (id_reg_B_0 == mem_dest_reg_1 && !id_reg_B_ZERO_0) begin
-            id_packet_out_0.rs2_select = RS_IS_MEM_1;
-        end
-        else if (id_reg_B_0 == mem_dest_reg_0 && !id_reg_B_ZERO_0) begin
-            id_packet_out_0.rs2_select = RS_IS_MEM_0;
-        end
-
-        if (id_reg_A_0 == ex_dest_reg_2 && !id_reg_A_ZERO_0) begin
-            id_packet_out_0.rs1_select = RS_IS_EX_2;
-        end
-        else if (id_reg_A_0 == ex_dest_reg_1 && !id_reg_A_ZERO_0) begin
-            id_packet_out_0.rs1_select = RS_IS_EX_1;
-        end
-        else if (id_reg_A_0 == ex_dest_reg_0 && !id_reg_A_ZERO_0) begin
-            id_packet_out_0.rs1_select = RS_IS_EX_0;
-        end
-        
-        if (id_reg_B_0 == ex_dest_reg_2 && !id_reg_B_ZERO_0) begin
-            id_packet_out_0.rs2_select = RS_IS_EX_2;
-        end
-        else if (id_reg_B_0 == ex_dest_reg_1 && !id_reg_B_ZERO_0) begin
-            id_packet_out_0.rs2_select = RS_IS_EX_1;
-        end
-        else if (id_reg_B_0 == ex_dest_reg_0 && !id_reg_B_ZERO_0) begin
-            id_packet_out_0.rs2_select = RS_IS_EX_0;
-        end
-
-        id_packet_out_1 = id_packet_1;
-        id_packet_out_1.rs1_select = RS_IS_RS;
-        id_packet_out_1.rs2_select = RS_IS_RS;
-        
-        if (id_reg_A_1 == mem_dest_reg_2 && !id_reg_A_ZERO_1) begin
-            id_packet_out_1.rs1_select = RS_IS_MEM_2;
-        end
-        else if (id_reg_A_1 == mem_dest_reg_1 && !id_reg_A_ZERO_1) begin
-            id_packet_out_1.rs1_select = RS_IS_MEM_1;
-        end
-        else if (id_reg_A_1 == mem_dest_reg_0 && !id_reg_A_ZERO_1) begin
-            id_packet_out_1.rs1_select = RS_IS_MEM_0;
-        end
-
-        if (id_reg_B_1 == mem_dest_reg_2 && !id_reg_B_ZERO_1) begin
-            id_packet_out_1.rs2_select = RS_IS_MEM_2;
-        end
-        else if (id_reg_B_1 == mem_dest_reg_1 && !id_reg_B_ZERO_1) begin
-            id_packet_out_1.rs2_select = RS_IS_MEM_1;
-        end
-        else if (id_reg_B_1 == mem_dest_reg_0 && !id_reg_B_ZERO_1) begin
-            id_packet_out_1.rs2_select = RS_IS_MEM_0;
-        end
-
-        if (id_reg_A_1 == ex_dest_reg_2 && !id_reg_A_ZERO_1) begin
-            id_packet_out_1.rs1_select = RS_IS_EX_2;
-        end
-        else if (id_reg_A_1 == ex_dest_reg_1 && !id_reg_A_ZERO_1) begin
-            id_packet_out_1.rs1_select = RS_IS_EX_1;
-        end
-        else if (id_reg_A_1 == ex_dest_reg_0 && !id_reg_A_ZERO_1) begin
-            id_packet_out_1.rs1_select = RS_IS_EX_0;
-        end
-
-        if (id_reg_B_1 == ex_dest_reg_2 && !id_reg_B_ZERO_1) begin
-            id_packet_out_1.rs2_select = RS_IS_EX_2;
-        end
-        else if (id_reg_B_1 == ex_dest_reg_1 && !id_reg_B_ZERO_1) begin
-            id_packet_out_1.rs2_select = RS_IS_EX_1;
-        end
-        else if (id_reg_B_1 == ex_dest_reg_0 && !id_reg_B_ZERO_1) begin
-            id_packet_out_1.rs2_select = RS_IS_EX_0;
-        end
-
-        id_packet_out_2 = id_packet_2;
-        id_packet_out_2.rs1_select = RS_IS_RS;
-        id_packet_out_2.rs2_select = RS_IS_RS;
-
-        if (id_reg_A_2 == mem_dest_reg_2 && !id_reg_A_ZERO_2) begin
-            id_packet_out_2.rs1_select = RS_IS_MEM_2;
-        end
-        else if (id_reg_A_2 == mem_dest_reg_1 && !id_reg_A_ZERO_2) begin
-            id_packet_out_2.rs1_select = RS_IS_MEM_1;
-        end
-        else if (id_reg_A_2 == mem_dest_reg_0 && !id_reg_A_ZERO_2) begin
-            id_packet_out_2.rs1_select = RS_IS_MEM_0;
-        end
-
-        if (id_reg_B_2 == mem_dest_reg_2 && !id_reg_B_ZERO_2) begin
-            id_packet_out_2.rs2_select = RS_IS_MEM_2;
-        end
-        else if (id_reg_B_2 == mem_dest_reg_1 && !id_reg_B_ZERO_2) begin
-            id_packet_out_2.rs2_select = RS_IS_MEM_1;
-        end
-        else if (id_reg_B_2 == mem_dest_reg_0 && !id_reg_B_ZERO_2) begin
-            id_packet_out_2.rs2_select = RS_IS_MEM_0;
-        end
-        
-        if (id_reg_A_2 == ex_dest_reg_2 && !id_reg_A_ZERO_2) begin
-            id_packet_out_2.rs1_select = RS_IS_EX_2;
-        end
-        else if (id_reg_A_2 == ex_dest_reg_1 && !id_reg_A_ZERO_2) begin
-            id_packet_out_2.rs1_select = RS_IS_EX_1;
-        end
-        else if (id_reg_A_2 == ex_dest_reg_0 && !id_reg_A_ZERO_2) begin
-            id_packet_out_2.rs1_select = RS_IS_EX_0;
-        end
-
-        if (id_reg_B_2 == ex_dest_reg_2 && !id_reg_B_ZERO_2) begin
-            id_packet_out_2.rs2_select = RS_IS_EX_2;
-        end
-        else if (id_reg_B_2 == ex_dest_reg_1 && !id_reg_B_ZERO_2) begin
-            id_packet_out_2.rs2_select = RS_IS_EX_1;
-        end
-        else if (id_reg_B_2 == ex_dest_reg_0 && !id_reg_B_ZERO_2) begin
-            id_packet_out_2.rs2_select = RS_IS_EX_0;
-        end
-
-    end
     
-    logic [1:0] mem_count;
-    
-    logic ex_load_0;
-    logic ex_load_1;
-    logic ex_load_2;
-
-    assign ex_load_0 = ex_packet_0.rd_mem;
-    assign ex_load_1 = ex_packet_1.rd_mem;
-    assign ex_load_2 = ex_packet_2.rd_mem;
-
     always_comb begin
-        rollback = 0;
+        logic [1:0] rollback_same_cycle, rollback_across_cycle;
+        rollback_across_cycle = 0;
+        rollback_same_cycle = 0;
         // hazards between stages
-        if (
-            (id_reg_A_0 == ex_dest_reg_0 && !id_reg_A_ZERO_0 && ex_load_0) ||
-            (id_reg_B_0 == ex_dest_reg_0 && !id_reg_B_ZERO_0 && ex_load_0) ||
-            (id_reg_A_0 == ex_dest_reg_1 && !id_reg_A_ZERO_0 && ex_load_1) ||
-            (id_reg_B_0 == ex_dest_reg_1 && !id_reg_B_ZERO_0 && ex_load_1) ||
-            (id_reg_A_0 == ex_dest_reg_2 && !id_reg_A_ZERO_0 && ex_load_2) ||
-            (id_reg_B_0 == ex_dest_reg_2 && !id_reg_B_ZERO_0 && ex_load_2)
-        ) begin
-            rollback = 3;
-        end
+        if (ex_load_depend_0)
+            rollback_across_cycle = 3;
+        else if (ex_load_depend_1)
+            rollback_across_cycle = 2;
+        else if (ex_load_depend_2)
+            rollback_across_cycle = 1;
+        
         // hazards between ways in single cycle
-        else if ((id_dest_reg_0 == id_reg_A_1 && !id_reg_A_ZERO_1) || (id_dest_reg_0 == id_reg_B_1 && !id_reg_B_ZERO_1)) begin
-            rollback = 2;
-        end
-        // hazards between stages
-        else if (
-            (id_reg_A_1 == ex_dest_reg_0 && !id_reg_A_ZERO_1 && ex_load_0) ||
-            (id_reg_B_1 == ex_dest_reg_0 && !id_reg_B_ZERO_1 && ex_load_0) ||
-            (id_reg_A_1 == ex_dest_reg_1 && !id_reg_A_ZERO_1 && ex_load_1) ||
-            (id_reg_B_1 == ex_dest_reg_1 && !id_reg_B_ZERO_1 && ex_load_1) ||
-            (id_reg_A_1 == ex_dest_reg_2 && !id_reg_A_ZERO_1 && ex_load_2) ||
-            (id_reg_B_1 == ex_dest_reg_2 && !id_reg_B_ZERO_1 && ex_load_2)
-        ) begin
-            rollback = 2;
-        end
-        // hazards between ways in single cycle
-        else if ((id_dest_reg_0 == id_reg_A_2 && !id_reg_A_ZERO_2) || (id_dest_reg_0 == id_reg_B_2 && !id_reg_B_ZERO_2)) begin
-            rollback = 1;
-        end
-        // hazards between ways in single cycle
-        else if ((id_dest_reg_1 == id_reg_A_2 && !id_reg_A_ZERO_2) || (id_dest_reg_1 == id_reg_B_2 && !id_reg_B_ZERO_2)) begin
-            rollback = 1;
-        end
-        // hazards between stages
-        else if (
-            (id_reg_A_2 == ex_dest_reg_0 && !id_reg_A_ZERO_2 && ex_load_0) ||
-            (id_reg_B_2 == ex_dest_reg_0 && !id_reg_B_ZERO_2 && ex_load_0) ||
-            (id_reg_A_2 == ex_dest_reg_1 && !id_reg_A_ZERO_2 && ex_load_1) ||
-            (id_reg_B_2 == ex_dest_reg_1 && !id_reg_B_ZERO_2 && ex_load_1) ||
-            (id_reg_A_2 == ex_dest_reg_2 && !id_reg_A_ZERO_2 && ex_load_2) ||
-            (id_reg_B_2 == ex_dest_reg_2 && !id_reg_B_ZERO_2 && ex_load_2)
-        ) begin
-            rollback = 1;
-        end
+        if (way_0_depend_by_1)
+            rollback_same_cycle = 2;
+        else if (way_0_depend_by_2 || way_1_depend_by_2)
+            rollback_same_cycle = 1;
+        
+        // the greater one
+        rollback=   (rollback_across_cycle > rollback_same_cycle) ? rollback_across_cycle :
+                    rollback_same_cycle;
     end
 
 endmodule
 
 `endif
-
